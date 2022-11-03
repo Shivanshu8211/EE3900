0625e93b532fe90c5b753e38ad8567532e05bbca
Shivanshu <ai21btech11027@iith.ac.in>
2022-11-03 10:38:24 +0530

    added

--- Circuit/codes/4.7.py
+++ Circuit/codes/4.7.py
@@ -1,13 +1,32 @@
 import numpy as np
+import scipy as sp
 import matplotlib.pyplot as plt
+import subprocess
+import shlex
 
-x=np.linspace(0,1e-5,300)
-y=5*(2/3)*(1-np.exp((-3/2)*1e6*x))
-plt.plot(x,y)
+def y_cont(t):
+    if t < 0: 
+        return 0
+    else:
+        return 2/3 * (1 - np.exp(-1.5e6 * t))
+
+def y_disc(n):
+    if n < 0:
+        return 0
+    else:
+        return 2/3 * (1 - (1 - 7.5e5 * 1e-7)**(n*1e7)/(1 + 7.5e5 * 1e-7)**(n*1e7))
+
+yt = sp.vectorize(y_cont)
+yn = sp.vectorize(y_disc)
+
+spice = np.loadtxt('4.7.dat')
+
+T = np.linspace(0, 5e-6, 50)
+
+plt.plot(T, yt(T), label='$y(t)$')
+plt.plot(spice[:,0], spice[:,1], 'o', label='ngspice')
+plt.plot(T, yn(T), '.', label='$y(n)$')
 plt.grid()
-ax=plt.gca()
-ax.set_xlabel('t')
-ax.set_ylabel('$y(t)$')
-# plt.savefig('Circuits/figs/2.7.eps')
-plt.savefig('Circuits/figs/2.7.pdf')
+plt.legend()
+plt.savefig('../figs/4.7.png')
 plt.show()
---
+++ Circuit/codes/bash.sh
@@ -0,0 +1,5 @@
+python3 2.4.py
+python3 2.5.py
+python3 2.7.py
+python3 2.8.py
+python3 3.4.py
